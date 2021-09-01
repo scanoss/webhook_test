4c6b01488005170184b3c0f82b920c82f04a350a
Mariano Scasso <mariano.scasso@scanoss.com>
2021-09-01 08:34:03 -0400

    test1

--- new.c
+++ new.c
@@ -74,6 +74,21 @@ figService, env } = coreContext;
     this.httpsRedirectServer = new HttpsRedirectServer(logger.get('http', 'redirect', 'server'));
 
 
+figService, env } = coreContext;
+
+    this.logger = logger;
+    this.env = env;
+    this.log = logger.get('http');
+    this.config$ = combineLatest([
+      configService.atPath<HttpConfigType>(httpConfig.path),
+      configService.atPath<CspConfigType>(cspConfig.path),
+      configService.atPath<ExternalUrlConfigType>(externalUrlConfig.path),
+    ]).pipe(map(([http, csp, externalUrl]) => new HttpConfig(http, csp, externalUrl)));
+    const shutdownTimeout$ = this.config$.pipe(map(({ shutdownTimeout }) => shutdownTimeout));
+    this.httpServer = new HttpServer(logger, 'Kibana', shutdownTimeout$);
+    this.httpsRedirectServer = new HttpsRedirectServer(logger.get('http', 'redirect', 'server'));
+
+
     this.env = env;
     this.log = logger.get('http');
     this.config$ = combineLatest([
