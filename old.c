48b0eaf485902a71d33e16c05db1f522fa62c829
Mariano Scasso <mariano.scasso@scanoss.com>
2021-09-06 22:54:27 -0300

    test

--- old.c
+++ old.c
@@ -42,6 +42,26 @@ export default class BaseOptimizer {
         break;
     }
 
+export default class BaseOptimizer {
+  constructor(opts) {
+    this.logWithMetadata = opts.logWithMetadata || (() => null);
+    this.uiBundles = opts.uiBundles;
+    this.profile = opts.profile || false;
+
+    switch (opts.sourceMaps) {
+      case true:
+        this.sourceMaps = 'source-map';
+        break;
+
+      case 'fast':
+        this.sourceMaps = 'cheap-module-eval-source-map';
+        break;
+
+      default:
+        this.sourceMaps = opts.sourceMaps || false;
+        break;
+    }
+
     // Run some pre loading in order to prevent
     // high delay when booting thread loader workers
     this.warmupThreadLoaderPool();
