36b387ac22e887285d71393ab685e0e0d365e957
mscasso-scanoss <75589700+mscasso-scanoss@users.noreply.github.com>
2021-09-21 09:50:07 -0400

    Update new.c

--- new.c
+++ new.c
@@ -54,3 +54,15 @@ his.logger = logger;
     const shutdownTimeout$ = this.config$.pipe(map(({ shutdownTimeout }) => shutdownTimeout));
     this.httpServer = new HttpServer(logger, 'Kibana', shutdownTimeout$);
     this.httpsRedirectServer = new HttpsRedire
+  }
+his.logger = logger;
+    this.env = env;
+    this.log = logger.get('http');
+    this.config$ = combineLatest([
+      configService.atPath<HttpConfigType>(httpConfig.path),
+      configService.atPath<CspConfigType>(cspConfig.path),
+      configService.atPath<ExternalUrlConfigType>(externalUrlConfig.path),
+    ]).pipe(map(([http, csp, externalUrl]) => new HttpConfig(http, csp, externalUrl)));
+    const shutdownTimeout$ = this.config$.pipe(map(({ shutdownTimeout }) => shutdownTimeout));
+    this.httpServer = new HttpServer(logger, 'Kibana', shutdownTimeout$);
+    this.httpsRedirectServer = new HttpsRedire
